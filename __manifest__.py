c0deb7ca12b572f18dc3f98cbd3353303b45001c
luissancar <luissancar@yahoo.es>
2019-01-07 12:38:54 +0100

    dos

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'tipos de datos',
+    'summary': 'módulotipos datos',
+    'author': 'Nosferatu',
+    'version': '1.0',
+    'website': 'iesayala.ddns.net',
+    'depends': ['base'],
+    'data': ['views/comunidad.xml']
+}
---
+++ models/tdatos.py
@@ -0,0 +1,24 @@
+
+from odoo import models, fields
+from datetime import datetime
+
+class Tdatos(models.Model):
+    _name = 'tiposdatos.tdatos'
+
+    def month_selection(self):
+        return [(1, 'Enero'), (2, 'Febrero'), (3, 'Marzo'), (4, 'Abril')]
+
+    char = fields.Char('Char',size=20, required=True)
+    texto = fields.Text('texto')
+    boolean = fields.Boolean('boolean')
+    html = fields.Html('html')
+    float_field = fields.Float(digits=lambda cr: (32, 2))
+    #parte entera, parte decimal
+    date = fields.Date('date')
+    datetime_field = fields.Datetime('datetime')
+    fecha_actual = fields.Date(string='Fecha actual', default=lambda s: fields.Date.context_today(s))
+    selection = fields.Selection(selection=month_selection, string='Mes',default=datetime.now().month, required=True)
+
+
+
+
